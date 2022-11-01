b3265aeca8b64526d05bd4aea994170aad138621
tmanti <tmanti17@gmail.com>
2022-11-01 00:01:15 -0400

    finish view transaction page

---
+++ src/pages/request/[id].tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+
+const ViewRequestPage: NextPage = () => {
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default ViewRequestPage;
--- src/pages/view/[id].tsx
+++ src/pages/view/[id].tsx
@@ -1,9 +1,41 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import { useRouter } from "next/router"
+import { useState } from "react";
+import { trpc } from "../../utils/trpc";
 
-const ViewPage: NextPage = () => {
+
+const ViewPage = () => {
+    const router = useRouter();
+
+    const [error, setError] = useState("");
+
+    const { data:transaction } = trpc.transaction.viewTransaction.useQuery({
+        transaction_id:router.query.id as string,
+    })
+
     return (
-        <></>
+        <>
+            <div>
+                <div>
+                    <span className="text-red-600 font-bold">{error}</span>
+                </div>
+                <div>
+                    <label>Name:</label><span>{transaction?.account_name}</span>
+                </div>
+                <div>
+                    <label>Start Rank: </label><span>{transaction?.start_rank}</span>
+                </div>
+                <div>
+                    <label>Desired Rank: </label><span>{transaction?.desired_rank}</span>
+                </div>
+                <div>
+                    <label>Status: </label><span>{transaction?.status}</span>
+                </div>
+                <div>
+                    <label>Notes: </label><span>{transaction?.notes}</span>
+                </div>              
+            </div>
+        </>
     )
 }
 
